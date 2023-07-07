0e34316649869fa10d65f86e25dfe64c2de1ccc5
Nozen <ramirezedelvalle@gmail.com>
2023-07-07 15:22:59 -0600

    Inicio

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea 1</title>
+</head>
+<body>
+    <h1>Ingenieria de Software II</h1>
+
+    <script src="./script.js"></script>
+</body>
+</html>
